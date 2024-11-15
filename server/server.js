e97a6215257d377396efdf7e810130d8acf45858
Pranavgawas1999 <pranavgawas@proton.me>
2024-11-15 07:01:47 +0000

    commit 8

--- server/server.js
+++ server/server.js
@@ -2,18 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();  // This line loads the environment variables
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('process.env.MONGO_URI', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('MongoDB connection error:', error));
+
 
 // Define a Link schema and model
 const linkSchema = new mongoose.Schema({ url: String });
