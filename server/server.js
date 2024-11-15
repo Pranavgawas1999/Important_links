8f6cfcc66cde3767eeb99203b2b3f4c36920043a
Pranavgawas1999 <pranavgawas@proton.me>
2024-11-15 13:50:24 +0000

    commit 9

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('process.env.MONGO_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
